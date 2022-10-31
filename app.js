000e291d56b648a05cae0595da6cea889eeb20de
Isaac Olanrewaju <isaacolanre@gmail.com>
2022-10-31 09:27:52 +0100

    removed the @ from my username

--- app.js
+++ app.js
@@ -9,12 +9,12 @@ const port = process.env.PORT || 8080;
 app.listen(port, () => console.log(`Listening on port ${port}..`));
 
 const data = {
-  slackUsername: "@isaacolanre",
+  slackUsername: "isaacolanre",
   backend: true,
   age: 31,
   bio: "I am a software engineer currently living in Lagos, Nigeria. My interests range from web development to mobile development. I am also interested in soccer, reading, and technology. I'm a full-stack engineer with a strong desire for pushing the boundaries, learning, and a healthy disregard for the impossible.",
 };
 
 app.get("/", (req, res) => {
-  res.send(JSON.stringify(data));
+  res.status(200).send(JSON.stringify(data));
 });
